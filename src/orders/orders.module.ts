eeaa763a23b769973369a044d2edb83c70babbf5
Gibson-Arbey <gibsonarbey@gmail.com>
2025-02-23 18:08:39 -0500

    se cambia la comunicacion TCP por NATS

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,24 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs } from 'src/config/envs';
-import { PRODUCT_SERVICE } from 'src/config/services';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { NatsModule } from 'src/nats/nats.module';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
   imports: [
-    ClientsModule.register([
-      {
-        name: PRODUCT_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: envs.productsMicroserviceHost,
-          port: envs.productsMicroservicePort,
-        },
-      },
-    ]),
+    NatsModule
   ]
 })
 export class OrdersModule {}
